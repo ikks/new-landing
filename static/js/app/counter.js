e6a6d48be90059d21775fab0e36f73688c0bf698
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2016-11-20 00:25:25 -0500

    - added method to hide countdown clock and city/event date container

--- static/js/app/counter.js
+++ static/js/app/counter.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     .on('update.countdown', function(event) {
         updateCounter(event.strftime('%D'), event.strftime('%H'), event.strftime('%M'), event.strftime('%S'));
     }).on('finish.countdown', function(event) {
-        $(this).html('This offer has expired!').parent().addClass('disabled');
+        hideCountDownAndCityContainer();
     });
 
     /**
@@ -25,7 +25,28 @@ $(document).ready(function(){
 
 });
 
+/**
+ * Hide counter, city/event and main logo text DOM elements
+*/
 function hideCountDownAndCityContainer(){
-    $('#clock').fadeOut();
+    $('#clock').fadeOut(400, setCounterTextAndShow);
     $('#city-event-date-container').fadeOut();
+    $('#main-logo-text').fadeOut();
+
+
+}
+
+/**
+ * Set ticket url and show elements
+*/
+function setCounterTextAndShow(){
+    $('#clockdiv').html('Tickets available now!!!');
+    $('#main-logo-text').html('Coding the future');
+    $('#city-event-date-container').addClass('blue-container-ticket');
+    $('#tickets-link').prop("href", "https://pyconcolombia.eventbrite.com");
+
+
+    $('#clock').fadeIn();
+    $('#city-event-date-container').fadeIn();
+    $('#main-logo-text').fadeIn();
 }
